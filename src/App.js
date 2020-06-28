da47bc3c49588d2bb26ea80394b34fd4e1281ed3
Aafaq Pervez <aafaqpervez@gmail.com>
2020-06-28 23:18:34 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React , {useState} from 'react';
+import {Values} from './values.js';
+import './App.css';
+
+function App() {
+
+  let [count, setCount]=useState(0)
+  let [IsMorning , setMorning]=useState(false)
+
+ 
+
+  return (
+    <div className={`box ${IsMorning ? 'dayLight': ''}`}>
+      <h1>Time ={IsMorning ?'Morning':'Night' }</h1>
+
+
+      <Values counter={count}/>
+
+  
+  <br/>
+  <button onClick={
+    ()=>setCount(++count)}>
+    Update counter
+  </button>
+  <br/>
+
+  <button onClick={()=>setMorning(!IsMorning)} >update Day </button>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/values.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Values(props)
+{
+    return(
+    <h3>the value of the counter varible is: {props.counter}</h3>
+
+    )
+}
